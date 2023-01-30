5f2c18b86b1892b3e4b7f9aebeb1f4040b131ac8
XIAO <swagshaw1998@gmail.com>
2023-01-30 16:28:59 +0800

    :sparkles: Init the project

---
+++ job.sh
@@ -0,0 +1,3 @@
+source activate audioset
+python process.py download -c "Knock" "Baby cry" "Fire alarm" "Fire" "Smoke alarm" "Car alarm" "Crash" "Breaking" \
+"Bark" "Groan" "Screaming" "Alarm" "Gunshot"
